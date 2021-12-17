19501932f0646ff730d33e38226b8367ee76d1ff
Anuar Ustayev <anuar.ustayev@gmail.com>
2021-12-17 12:17:43 +0600

    Updated list of projects I was part of.

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,63 @@
 const projectsData = [
   {
+    title: 'US Government\'s Open Data',
+    description: 'The home of the U.S. Government’s open data.',
+    imgSrc: '/static/images/data.gov.png',
+    href: 'https://www.data.gov/',
+  },
+  {
+    title: 'National Health Service (UK)',
+    description: 'Open Data Portal for the NHS Business Services Authority (NHSBSA).',
+    imgSrc: '/static/images/nhs.png',
+    href: 'https://opendata.nhsbsa.net/',
+  },
+  {
+    title: 'Denmark Open Data',
+    description: 'Open Data from public sector of Denmark.',
+    imgSrc: '/static/images/oddk.png',
+    href: 'https://www.opendata.dk/',
+  },
+  {
+    title: 'Northern Ireland Open Data',
+    description: 'Easy access to Northern Ireland public sector data for both reuse and redistribution.',
+    imgSrc: '/static/images/odni.png',
+    href: 'https://www.opendatani.gov.uk/',
+  },
+  {
+    title: 'CKAN Cloud',
+    description: 'Managed CKAN in the cloud.',
+    imgSrc: '/static/images/ckan.png',
+    href: 'https://www.datopian.com/pricing/',
+  },
+  {
     title: 'Denmark Energy Data Portal',
     description: 'EnergiDataPortal intends to embrace all Energinet’s initiatives that deal with open data, whether it be display, use or inspiration.',
-    imgSrc: '/static/images/default.png',
+    imgSrc: '/static/images/edp.svg',
     href: 'https://energidataportal.dk/',
   },
   {
     title: 'Denmark Energy Data Service',
     description: 'Open energy data from Energinet to society',
-    imgSrc: '/static/images/default.svg',
+    imgSrc: '/static/images/eds.png',
     href: 'https://www.energidataservice.dk/',
+  },
+  {
+    title: 'National Grid ESO Data Portal',
+    description: 'Open data from Great Britain’s Electricity System Operator.',
+    imgSrc: '/static/images/nationalgrid.png',
+    href: 'https://data.nationalgrideso.com/',
+  },
+  {
+    title: 'Montréal Open Data',
+    description: 'Information assets that are important both to the city and to citizens.',
+    imgSrc: '/static/images/montreal.png',
+    href: 'https://donnees.montreal.ca/',
+  },
+  {
+    title: 'New Mexico Water Data',
+    description: 'A collection of water data - for effective water management and planning.',
+    imgSrc: '/static/images/newmexicowaterdatalogo.png',
+    href: 'https://catalog.newmexicowaterdata.org/',
   },
   {
     title: 'Open and Standard datasets for Kazakhstan',
@@ -25,7 +73,7 @@ const projectsData = [
   },
   {
     title: 'Core Datasets',
-    description: `Important, commonly-used datasets in high quality, easy-to-use & open form as data packages`,
+    description: `Important, commonly-used datasets in high quality, easy-to-use & open form as data packages.`,
     imgSrc: '/static/images/open-data.png',
     href: 'https://github.com/datasets',
   },
@@ -37,7 +85,7 @@ const projectsData = [
   },
   {
     title: 'Data CLI',
-    description: `Command line tool for working with data, Data Packages and the DataHub`,
+    description: `Command line tool for working with data, Data Packages and the DataHub.`,
     imgSrc: '/static/images/data-factory.jpeg',
     href: 'https://github.com/datopian/data-cli',
   },
@@ -46,6 +94,12 @@ const projectsData = [
     description: `A lightweight, standardized library accessing files and datasets, especially tabular ones (CSV, Excel).`,
     imgSrc: '/static/images/frictionless-logo.png',
     href: 'https://github.com/frictionlessdata/frictionless-js',
+  },
+  {
+    title: 'State of carbon',
+    description: 'The dashboard displaying state of PM2.5 globally.',
+    imgSrc: '/static/images/carbon.png',
+    href: 'https://carbon.datahub.io/',
   },
   {
     title: 'London - City Dashboard',
